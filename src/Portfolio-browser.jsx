7bf9638f3664fff2f5aaa51da2d30c68b724d62b
c3c1l1a <cesswangumukima@gmail.com>
2022-12-10 17:08:39 +0800

    Add a template

---
+++ src/Portfolio-browser.jsx
@@ -0,0 +1,40 @@
+import {katnip} from "katnip";
+
+// This file will be run in the browser.
+// Here we do things like:
+
+// Add custom elements, see: https://katnip.tech/page/custom-elements
+// katnip.addElement(/*...*/);
+
+// Add routes, see: https://katnip.tech/page/creating-routes
+// katnip.addRoute(/*...*/);
+
+// Add page templates.
+// katnip.addTemplate(/*...*/);
+
+// We can communicate with the server using
+// katnip.apiFetch(/*...*/); or katnip.useApiFetch(/*...*/);
+
+// Or consume data from a realtime channel, see: https://katnip.tech/page/realtime-channels
+// katnip.useChannel(/*...*/);
+
+// Hook into the system in various ways, listen for events
+// to customize the client side behaivour.
+// katnip.addAction(/*...*/);
+
+katnip.addTemplate("**",({children})=>{
+  let menu=katnip.useChannel("menuHeader");
+  console.log(menu);
+  return (
+  	<div>
+  		hello... {children}
+  	</div>
+  );
+});
+
+katnip.addAction("getMenuLocations",(items)=>{
+	items.push({
+		title: "Header Menu",
+		setting: "menuHeader"
+	});
+})
